966c9f8ebe575d0ca6e6e0e54694c5ef76a6679c
iamcody-git <iamcody.github@gmail.com>
2025-10-10 14:30:40 +0545

    first commit

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from '../Components/Home/Banner'
+import Hero from '../Components/Home/Hero'
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Hero />
+    </>
+  )
+}
+
+export default Home
---
+++ client/src/Pages/ResumeBuilder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResumeBuilder = () => {
+  return (
+    <div>
+      <h1>resume builder</h1>
+    </div>
+  )
+}
+
+export default ResumeBuilder
