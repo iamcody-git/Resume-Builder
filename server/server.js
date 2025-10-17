83eb30dcf2860026d700fbc912f235709ac98ac5
iamcody-git <iamcody.github@gmail.com>
2025-10-17 09:17:23 +0545

    api with ai is done

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from "./configs/Db.js";
 import userRouter from "./routes/userRoutes.js";
 import resumeRouter from "./routes/resumeRoutes.js";
+import aiRouter from "./routes/aiRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,8 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("Server is running..."));
 app.use('/api/users', userRouter)
 app.use('/api/resumes', resumeRouter)
+
+app.use('/api/ai', aiRouter)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
