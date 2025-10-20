9f752640194dc738e1e508e994c341b69b4ec0d0
iamcody-git <iamcody.github@gmail.com>
2025-10-20 12:00:23 +0545

    chnages in preview solve shre issue

--- client/src/Pages/Preview.jsx
+++ client/src/Pages/Preview.jsx
@@ -6,47 +6,50 @@ import { ArrowLeftIcon } from 'lucide-react'
 import api from '../configs/api'
 
 const Preview = () => {
-  const {resumeId} = useParams()
-  const [isLoading,setIsLoading] = useState(true)
-
+  const { resumeId } = useParams()
+  const [isLoading, setIsLoading] = useState(true)
   const [resumeData, setResumeData] = useState(null)
-  const loadResume = async ()=>{
+
+  const loadResume = async () => {
     try {
-      const {data} = await api.get('/app/resumes/public/' + resumeId)
+      const { data } = await api.get(`/api/resumes/public/${resumeId}`)
       setResumeData(data.resume)
     } catch (error) {
-      console.log(error.message);
-
-      
-    }finally{
+      console.error('Error loading resume:', error)
+    } finally {
       setIsLoading(false)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     loadResume()
-  },[])
+  }, [resumeId])
 
-  return resumeData?  (
-    <div className='bg-slate-100'>
-      <div className='max-w-3xl mx-auto py-10'>
-        <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} classes='py-4 bg-white' />
+  if (isLoading) return <Loader />
 
+  return resumeData ? (
+    <div className="bg-slate-100 min-h-screen">
+      <div className="max-w-3xl mx-auto py-10">
+        <ResumePreview
+          data={resumeData}
+          template={resumeData.template}
+          accentColor={resumeData.accent_color}
+          classes="py-4 bg-white"
+        />
       </div>
-      
     </div>
-  ):(
-    <div>
-      {isLoading ? <Loader /> :(
-        <div className='flex flex-col items-center justify-center h-screen'>
-          <p className='text-center text-5xl text-slate-400 font-medium'>Resume not found</p>
-          <a href="/" className='mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors'>
-            <ArrowLeftIcon className='mr-2 size-4' /> Go to home page
-          </a>
-        </div>
-      )}
+  ) : (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="text-center text-5xl text-slate-400 font-medium">
+        Resume not found
+      </p>
+      <a
+        href="/"
+        className="mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors"
+      >
+        <ArrowLeftIcon className="mr-2 size-4" /> Go to home page
+      </a>
     </div>
-
   )
 }
 
